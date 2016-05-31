1f7a42b8c07eae7a8ca4bacfd91ba1e3ad184525
manu <manu@localhost>
2016-05-31 22:18:15 +0200

    - Renombrados los String que recibe el metodo update a "condicion" y "vCondicion" - No se han añadido conexion.close(); a los metodos. A excepcion del metodo "select". - Metodo select creado y comentado.

--- src/acceso/AccesoDatos.java
+++ src/acceso/AccesoDatos.java
@@ -3,7 +3,11 @@ package acceso;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -101,19 +105,19 @@ public class AccesoDatos {
      * El primer String "campo" es el nombre del campo que queremos actualizar en la tabla de la BD, el segundo String "campo" es el valor del anterior String "campo" que actualizaremos.
      * Por cada String "campo" que añadiremos tendremos que añladir otro mas con su valor.
      * @param updateFrom Nombre de la tabla en la BD donde queremos hacer el update.
-     * @param campoBusqueda Campo por el que buscamos para actualizar una fila. Preferible que sea una primary key.
-     * @param vCampoBusqueda Valor que le daremos a "vCampoBusqueda".
+     * @param condicion Campo por el que buscamos para actualizar una fila. Preferible que sea una primary key.
+     * @param vCondicion Valor que le daremos a "condicion".
      * @param campo Campos que actualizaremos en tabla de la BD. Primer String nombre del campo, segundo String valor del campo.
      * @return El número de filas actualizadas. 0 = No actualizadas.
      */
-    public int update(String updateFrom, String campoBusqueda, String vCampoBusqueda, String... campo){
+    public int update(String updateFrom, String condicion, String vCondicion, String... campo){
         try {
             conexion = DriverManager.getConnection("jdbc:mysql://"+host+"/"+baseDatos, user, pass);
             String query = "update " + updateFrom + " set " + campo[0] + "= ?";
             for(int i=2; i<campo.length; i=i+2){
                 query += ", " + campo[i] + "= ?";
             }
-            query += " where " +  campoBusqueda + "= ?";
+            query += " where " +  condicion + "= ?";
             for (int i=1; i<campo.length; i=i+2){
                 
             }
@@ -123,12 +127,46 @@ public class AccesoDatos {
                 preparedStmt.setString (a, campo[i]);
                 a++;
             } 
-            preparedStmt.setString (a, vCampoBusqueda);
+            preparedStmt.setString (a, vCondicion);
             System.out.println("Actualización realizada");
             return preparedStmt.executeUpdate();
         } catch (SQLException ex) {
             System.out.println("Error de Actualización: " + ex.getMessage());
             return 0;
+        }
+    }
+    /**
+     * Crea una consulta para hacer una busqueda con los parametros que se le introduce.
+     * El primer parametro es el nombre de la jTable en la que queremos mostrar los resultados del select.
+     * El segundo parametro es el nombre de la tabla de la BD sobre la que queremos hacer la busqueda.
+     * Introducir tantos String campo como columnas tengamos en la jTable y resultados de la consulta querramos saber.
+     * @param tabla Nombre de la jTable en la que queremos mostrar los resultados del select.
+     * @param selectFrom Nombre de la tabla en la BD donde queremos hacer el select.
+     * @param condicion Campo por el que buscamos filas.
+     * @param vCondicion Valor que le daremos a "condicion".
+     * @param campo Campos del resultado de la busqueda que mostraremos en jTable. El numero de campos tiene que ser similar a las columnas de la jTable.
+     */
+    public void select (JTable tabla, String selectFrom, String condicion, String vCondicion, String... campo){
+        DefaultTableModel modelo = (DefaultTableModel) tabla.getModel();
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://"+host+"/"+baseDatos, user, pass);
+            Statement s = conexion.createStatement();
+            String query = "select " + campo[0];
+            for (int i=1; i<campo.length; i++){
+                query += ", "+ campo[i];
+            }
+            query += " from " + selectFrom + " where " + condicion + " like '%" + vCondicion +"%'";
+            ResultSet consulta = s.executeQuery(query);
+            while (consulta.next()) {
+                Object [] filaTabla = new Object [campo.length];
+                for(int i=0; i<campo.length; i++){
+                    filaTabla [i] = consulta.getString(i+1);
+                }
+                modelo.addRow(filaTabla);
+            }
+            conexion.close();
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
         }
     }
 }

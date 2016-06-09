9e331a1e2e18eff77fe214bc6e1291a158eb040d
manu <manu@localhost>
2016-06-09 12:18:26 +0200

    - Metodos traerImagen y limpiarTabla creados y comentados.

--- src/acceso/AccesoDatos.java
+++ src/acceso/AccesoDatos.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -168,5 +170,30 @@ public class AccesoDatos {
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
         }
+    }
+    /**
+     * Borra los campos de una jTable que le introduzcamos.
+     * @param tabla nombre de la jTable que queremos borrar.
+     */
+    public void limpiarTabla (JTable tabla) {
+        try {
+            DefaultTableModel modelo=(DefaultTableModel) tabla.getModel();
+            int filas=tabla.getRowCount();
+            for (int i = 0;filas>i; i++) {
+                modelo.removeRow(0);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al limpiar la tabla.");
+        }
+    }
+    /**
+     * Carga una imagen en una jLabel.
+     * @param caratula nombre de la jlabel en la que queremos mostrar la imagen.
+     * @param titulo nombre de la imagen quq queremos cargar
+     * @param ruta ruta donde esta la imagen.
+     */
+    public void traerImagen (JLabel caratula, String titulo, String ruta){
+        ImageIcon imagen = new ImageIcon(this.getClass().getResource(ruta+titulo+".jpg"));
+        caratula.setIcon(imagen);
     }
 }
